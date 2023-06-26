c5d1c89438e87d97789779856f16e769b2c8f99c
PierpaoloS <serrano.pierpaolo96@yahoo.it>
2023-06-26 15:11:16 +0200

    Test su Lightning e alcune modifiche sulla scena

--- Assets/VideoTrigger.cs
+++ Assets/VideoTrigger.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class VideoTrigger : MonoBehaviour
 {
     public GameObject tvLight;
-    //public GameObject videoPlayer;
+    public GameObject videoPlayer;
     // Start is called before the first frame update
     void Start()
     {
         tvLight.SetActive(false);
+        videoPlayer.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,8 +23,9 @@ public class VideoTrigger : MonoBehaviour
     {
         if (other.gameObject.CompareTag("TVCircle"))
         {
-            Debug.Log("Triggera Luce");
+            Debug.Log("Triggera Luce & Lancia Video");
             tvLight.SetActive(true);
+            videoPlayer.SetActive(true);
         }
         
     }
